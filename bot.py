cd606ddc70f92311a08eaea46ea6c1b46a88bd39
Wambugu Kinyua <95144881+wambugu71@users.noreply.github.com>
2023-02-13 01:36:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,17 +2,22 @@
 import telebot
 import requests
 import yt_dlp
+import time
 import os
 import json, youtube_dl
+#TELEGRAM_TOKEN= 5686577136:AAF8NGC6p-Jqw17XWCL4Z-7DW9WafzsVHzY
 #Config vars
 #token = os.environ['TELEGRAM_TOKEN']
-
-#with open('config.json') as f:
+#token = os.environ['5686577136:AAF8NGC6p-Jqw17XWCL4Z-7DW9WafzsVHzY']
+TOKEN= "5686577136:AAF8NGC6p-Jqw17XWCL4Z-7DW9WafzsVHzY"
+#	with open('config.json') as f:
 # token = json.load(f)
-TOKEN = "5686577136:AAF8NGC6p-Jqw17XWCL4Z-7DW9WafzsVHzY"
+  
 #Intitialize YouTube downloader
 ydl_opts = {}
 ydl = yt_dlp.YoutubeDL(ydl_opts)
+
+
 
 #initialise  bot
 #bot = telebot.TeleBot(token)
@@ -25,7 +30,7 @@ print(x)
 # works when /start is given
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-  bot.send_message(message.chat.id, "Welcome user this is a YouTube downloader made by @wambugu_kinyua.\n Usage type:\t1. '/youtube <link>'\n\t2. '/facebook <video/reel link>")
+  bot.send_message(message.chat.id, "Welcome user this is a YouTube downloader made by @wambugu_kinyua.\n Usage:\n /youtube <link>\n /Facebook <link> to download videos and reels")
 
 # works when /motivate is given
 @bot.message_handler(commands=['motivate'])
@@ -34,7 +39,7 @@ def send_quotes(message):
         bot.send_message(message.chat.id, quote.json()['content'])
 
 # works when /ytdl <link> is given
-@bot.message_handler(commands=['youtube'])
+@bot.message_handler(commands=['youtube', 'Youtube', 'YouTube'])
 def down(msg):
     args = msg.text.split()[1]
     try:
@@ -66,5 +71,36 @@ def down(msg):
 
     except:
        bot.reply_to(msg, ' sorry This can\'t be downloaded by me')
+@bot.message_handler(commands=["facebook","Facebook"])
+def facebook(message):
+    args = message.text.split()[1]
+    #bot.send_message(message.chat.id, text= os.system(f"youtube-dl {args} -g") )
+    ydl_opts = {}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+ #       yvd=ydl.download([args]
+        try:
+            with ydl:
+                info = ydl.extract_info( args,download=False)
+
+            if 'entries' in info:
+                # Can be a playlist or a list of videos
+                video = info['entries'][0]
+            else:
+                # Just a video
+                video = info
+            
+            for i in video['formats']:
+                link = '<a href=\"' + i['url'] + '\">' + 'link' + '</a>'
+    
+                if i.get('format_note'):
+                    bot.reply_to(message, 'Quality- ' + i['format_note'] + ': ' + link, parse_mode='HTML')
+                else:
+                    bot.reply_to(message, link, parse_mode='HTML', disable_notification=True)
+        except:
+            bot.reply_to(message, ' sorry This can\'t be downloaded by me')
+@bot.message_handler(commands=['start','hello' 'help'])
+def send_welcome(message):
+  bot.send_message(message.chat.id, "use command /start to welcome menu. Use /youtube <link> to download the video, choose your best quality to download.Enjoy your favorite videos.")
+
 #pool~start the bot
 bot.polling()
